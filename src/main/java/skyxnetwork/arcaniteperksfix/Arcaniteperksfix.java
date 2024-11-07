802cb0a1247be2450d53fc56651bbda61f672160
XPaladiumyX <allegra.yann@gmail.com>
2024-11-07 22:11:46 +0100

    Fix Support for 1.20.1

---
+++ src/main/java/skyxnetwork/arcaniteperksfix/Arcaniteperksfix.java
@@ -0,0 +1,17 @@
+package skyxnetwork.arcaniteperksfix;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Arcaniteperksfix extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
