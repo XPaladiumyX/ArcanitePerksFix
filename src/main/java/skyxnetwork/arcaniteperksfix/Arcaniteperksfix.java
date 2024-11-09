adf566c688364ab1975320c889459ea6ed88dfdc
XPaladiumyX <allegra.yann@gmail.com>
2024-11-09 14:43:38 +0100

    Removed Message on equi/unequip + Effect Timer Increased

--- src/main/java/skyxnetwork/arcaniteperksfix/Arcaniteperksfix.java
+++ src/main/java/skyxnetwork/arcaniteperksfix/Arcaniteperksfix.java
@@ -1,11 +1,9 @@
 package skyxnetwork.arcaniteperksfix;
 
-import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent;
 import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -34,18 +32,7 @@ public final class Arcaniteperksfix extends JavaPlugin implements Listener {
     public void onDisable() {
         // Code d'arrêt du plugin si nécessaire
     }
-
-    @EventHandler
-    public void onArmorChange(PlayerArmorChangeEvent event) {
-        // Événement appelé quand le joueur change d'armure
-        ItemStack newChestplate = event.getNewItem();
-        if (isCustomLeatherChestplate(newChestplate)) {
-            event.getPlayer().sendMessage("§aEffet de résistance au feu appliqué !");
-        } else {
-            event.getPlayer().sendMessage("§cEffet de résistance au feu retiré.");
-        }
-    }
-
+    
     private boolean isCustomLeatherChestplate(ItemStack item) {
         return item.getType() == Material.LEATHER_CHESTPLATE &&
                 item.getItemMeta() != null &&
@@ -60,7 +47,7 @@ public final class Arcaniteperksfix extends JavaPlugin implements Listener {
                 ItemStack chestplate = player.getInventory().getChestplate();
                 if (chestplate != null && isCustomLeatherChestplate(chestplate)) {
                     // Applique l'effet de résistance au feu pendant 0,1 seconde (2 ticks)
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 2, 0, true, false, false));
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 60, 0, true, false, false));
                 }
             }
         }
